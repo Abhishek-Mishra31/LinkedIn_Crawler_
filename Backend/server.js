bacbf5fb7c5f0035e4b08a98bc5d0af7ff433f28
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2024-12-27 00:12:32 +0530

    prod_finally

--- Backend/server.js
+++ Backend/server.js
@@ -36,10 +36,7 @@ app.post("/scrape", async (req, res) => {
 
     browser = await puppeteer.launch({
       headless: true,
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? "/opt/render/.cache/puppeteer/chrome/linux-131.0.6778.204/chrome-linux64/chrome"
-          : puppeteer.executablePath(),
+      executablePath: puppeteer.executablePath(),
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
